1b7305faf9bbba633c953eca02cf330b9b65de8d
Samuel Matthew <samuelmatthew211@gmail.com>
2021-11-12 12:57:37 +0700

    :zap: Wire CoursesPage & CourseDetailPage

--- src/views/CoursesPage.vue
+++ src/views/CoursesPage.vue
@@ -62,11 +62,6 @@
               :key="course.id"
               :course="course"
             />
-            <CourseCard
-              v-for="course in courses"
-              :key="course.id"
-              :course="course"
-            />
           </div>
         </div>
       </div>
@@ -81,34 +76,7 @@ export default {
   name: "CoursesPage",
   data: function () {
     return {
-      courses: [
-        {
-          id: 1,
-          name: "Program Linear",
-          description:
-            "Program Linear adalah bagian matematika yang membahas cara menentukan nilai optimum suatu fungsi – dinamakan fungsi obyektif atau fungsi tujuan – pada suatu sistem yang dibatasi beberapa kendala./nAda dua macam nilai optimum yang akan ditentukan, yaitu nilai maksimum dan nilai minimum.",
-          price: 145000,
-          thumbnailUrl:
-            "https://i.ytimg.com/vi/WJr11FExG7s/hqdefault.jpg?s…RUAAIhCGAE=&rs=AOn4CLAImD8rRbQR7cuFCw3Z_xsjLlr1Tg",
-          difficulty: "medium",
-          status: "active",
-          rating: 8,
-          CategoryId: 1,
-        },
-        {
-          id: 2,
-          name: "Dimensi Tiga",
-          description:
-            "Yang dimaksud dengan unsur-unsur ruang adalah titik, garis, dan bidang./nDalam Geometri, titik, garis, dan bidang, termasuk dalam aksioma atau postulat./nAksioma adalah pernyataan yang diterima sebagai kebenaran secara umum tanpa perlu adanya pembuktian.",
-          price: 130000,
-          thumbnailUrl:
-            "https://i.ytimg.com/vi/strzhKqA_sQ/hqdefault.jpg?s…RUAAIhCGAE=&rs=AOn4CLCVGKk89pHJwWo1ORt8hzlH1iM9rg",
-          difficulty: "easy",
-          status: "active",
-          rating: 8,
-          CategoryId: 1,
-        },
-      ],
+      courses: [],
       categories: [
         {
           id: 1,
@@ -156,21 +124,23 @@ export default {
       this.name = "";
     },
     fetchCoursesUser() {
-      // const payload = {
-      //   categoryId: this.categoryId,
-      //   orderBy: this.orderBy,
-      //   name: this.name,
-      //   page: this.page,
-      // };
-      // console.log(payload);
-      // this.$store
-      //   .dispatch("fetchCoursesUser", payload)
-      //   .then((result) => {
-      // this.courses = result;
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
-      //   });
+      const payload = {
+        categoryId: this.categoryId,
+        orderBy: this.orderBy,
+        name: this.name,
+        page: this.page,
+      };
+
+      this.$store
+        .dispatch("fetchCoursesUser", payload)
+        .then((result) => {
+          console.log(result);
+          this.courses = result.course;
+          // this.courses = result;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
   created() {
